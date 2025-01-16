0802f1338bae3d174ada3f780ee261f5d2598b55
Joshua_Williams <joshuawilliams11@mail.weber.edu>
2025-01-16 10:03:42 -0700

    Add: Docstring for program info

--- AdventureGame.py
+++ AdventureGame.py
@@ -1,3 +1,13 @@
-# adventure_game
+'''
+Adventure Game
+Author: Joshua Williams
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+'''
+
+
+# Welcome Message and Introduction
 print("Welcome to the Adventure Game")
 print("Your journey begins here...")
