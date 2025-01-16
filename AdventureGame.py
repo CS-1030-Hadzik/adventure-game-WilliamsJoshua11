99eff91cf193441c028d4415da27c49424c40a3f
Joshua_Williams <joshuawilliams11@mail.weber.edu>
2025-01-16 09:35:29 -0700

    Add: Author Information

--- AdventureGame.py
+++ AdventureGame.py
@@ -1,2 +1,4 @@
-print("Welcome to the Adventure Game")
-print("Your journey begins here...")
+# adventure_game
+this is the starter code that we will be using to learn python, git, and vscode
+
+# this game was created by Joshua Williams
