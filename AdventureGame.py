124c5448a39ebd676a31e6d3c6a12a79b33dec3e
Joshua_Williams <joshuawilliams11@mail.weber.edu>
2025-01-23 10:05:47 -0700

    ADD: code lines 15-19

--- AdventureGame.py
+++ AdventureGame.py
@@ -11,3 +11,9 @@ to navigate through a mysterious forest.
 # Welcome Message and Introduction
 print("Welcome to the Adventure Game")
 print("Your journey begins here...")
+
+# Ask for Player's name
+player_name = input("What is your name, adventururer?")
+
+#Concatenate strings to create a personalized message
+print("Welcome, " + player_name + "! Your journey begins now.")
