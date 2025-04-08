2959250ef1990ca6f8defa534f370fb3a86ab099
Joshua_Williams <joshuawilliams11@mail.weber.edu>
2025-04-08 12:48:18 -0600

    REF: Game state flags implemented

--- AdventureGame.py
+++ AdventureGame.py
@@ -98,18 +98,3 @@ while True:
         break
 
 
-
-# TODO: In path 1, after picking up the lantern:
-#       - Set player.has_lantern = True
-
-# TODO: In path 2, after picking up the map:
-#       - Set player.has_map = True
-
-# TODO: (Optional Stretch) Add a check before certain choices
-#       - Example: If player.has_lantern is False, prevent entering a cave
-#       - Print a message like “It’s too dark to continue without a lantern.”
-
-# TODO: Update all print statements that used player_name to use player.name
-
-# TODO: Commit and push your code with a message like:
-#       REF player class added and game state flags implemented
